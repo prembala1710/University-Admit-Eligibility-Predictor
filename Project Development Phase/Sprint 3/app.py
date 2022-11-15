20540d474ad4aabcdb85c203998035d49c203706
Prem B <prembala1710@gmail.com>
2022-11-15 08:00:39 +0530

    Updating Styles of Application
    Updating Styles of University Admit Eligibility Predictor Application

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 
 app = Flask(__name__,static_url_path='')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
+
+# No cacheing at all for API endpoints.
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    if 'Cache-Control' not in response.headers:
+        response.headers['Cache-Control'] = 'no-store'
+    return response
+
 CORS(app)
 
 @app.route('/',methods=['GET'])
@@ -17,9 +27,8 @@ def PredictPossibility():
     SOP = float(request.form['SOP'])
     LOR = float(request.form['LOR'])
     CGPA = float(request.form['CGPA'])
-    ResearchText = request.form['Research']
     Research = 0
-    if(ResearchText == 'done'):
+    if('Research' in request.form):
         Research = 1
     X = [[GREScore , TOEFLScore , UnivRating , SOP , LOR , CGPA , Research ]]
     model = joblib.load('model/Admission_Predictor.pkl')
@@ -31,17 +40,21 @@ def PredictPossibility():
     probability = (target_scale.inverse_transform(model.predict(X))[0][0].round(2)) *100
     print(probability)
     prob_comment = ""
+    color_scheme = ""
     if(probability > 100):
         probability = 100
     elif(probability < 0):
         probability = 0
     if(probability < 50):
         prob_comment = "The Chances of Getting an Admission is less likely"
+        color_scheme = 'darkorange'
     elif(probability < 70):
-        prob_comment = "There is a slight Chance of Possibility.Give it a Try"
+        prob_comment = "There is a slight Chance of Possibility."
+        color_scheme = 'yellow'
     else:
         prob_comment = "There is High Chances of Possibility"
-    return render_template('predict.html',predict=probability,comment=prob_comment)
+        color_scheme = 'lawngreen'
+    return render_template('predict.html',predict=probability,comment=prob_comment,color_scheme=color_scheme)
 
 if __name__ == '__main__':
     app.run(debug = True)
